ba4bd2a51c90fa6fa5f0391a6327aa4e10a620fb
katharine-atx <rovinsky.katharine@gmail.com>
2016-05-12 15:01:44 -0500

    Updated logistic regression with and without regularization

--- ex2_regularized_complete.m
+++ ex2_regularized_complete.m
@@ -1,63 +1,18 @@
-% Machine Learning Online Class - Exercise 2: Logistic Regression
-% Part 2: Regularization
-
-% Assignment:
-% Implement regularized logistic regression to predict whether microchips 
-% from a fabrication plant passes quality assurance (QA). You have the
-% test results for some microchips on two different tests. From these two tests,
-% you would like to determine whether the microchips should be accepted or
-% rejected. To help you make the decision, you have a dataset of test results
-% on past microchips, from which you can build a logistic regression model.
+%% Machine Learning Online Class - Exercise 2: Logistic Regression
+%  Part 2: Adding regularization...
 
 %% Initialization
 clear ; close all; clc
 
-%% Load Data
-data = load('ex2data2.txt');
-%  The first two columns contains the X values and the third column
-%  contains the label (y).
-X = data(:, [1, 2]); 
-y = data(:, 3);
+% Functions...
 
-%PLOTDATA Plots the data points X and y into a new figure 
-%   PLOTDATA(x,y) plots the data points with + for the positive examples
-%   and o for the negative examples. X is assumed to be a Mx2 matrix.
+function g = sigmoid(z)
+	% Initialize g... 
+	g = zeros(size(z));
+	
+	g = 1./(1 + e.^(-z));
 
-function plotData(X, y)
-	% Create New Figure
-	figure; hold on;
-
-	% Find Indices of Positive and Negative Examples
-	pos = find(y==1); neg = find(y == 0);
-
-	% Plot Examples
-	plot(X(pos, 1), X(pos, 2), 'k+','LineWidth', 2, ...
-	'MarkerSize', 7);
-	plot(X(neg, 1), X(neg, 2), 'ko', 'MarkerFaceColor', 'y', ...
-	'MarkerSize', 7);
-	hold off;
-
-end
-
-plotData(X, y);
-
-hold on;
-% Labels and Legend
-xlabel('Microchip Test 1')
-ylabel('Microchip Test 2')
-legend('y = 1', 'y = 0')
-hold off;
-
-
-% Part 1: Regularized Logistic Regression
-% The decision boundary does not appear linear. Adding polynomial features 
-% to the data matrix in order to use logistic regression effectively...
-
-% MAPFEATURE(X1, X2) maps the two input features
-% to quadratic features used in the regularization exercise.
-% Returns a new feature array with X1, X2, X1.^2, X2.^2, X1*X2, 
-% X1*X2.^2, etc.. as well as the intercept vector (ones)
-% Note: Requires that X1 and X2 inputs be the same size
+end;
 
 function out = mapFeature(X1, X2)
 
@@ -71,55 +26,41 @@ function out = mapFeature(X1, X2)
 
 end
 
-X = mapFeature(X(:,1), X(:,2));
-
-% Initialize fitting parameters
-initial_theta = zeros(size(X, 2), 1);
-m = length(y)
-
-% Set regularization parameter lambda to 1
-lam = 1;
-
-% J = SIGMOID(z) computes the sigmoid transformation for predictions z
-% Note z can be a matrix, vector or scalar.
-
-function g = sigmoid(z)
-	% Initialize g... 
-	g = zeros(size(z));
-	g = 1./(1 + e.^(-z));
-
-end
-
+% Regularized cost function....
 function [J, grad] = costFunctionReg(theta, X, y, lambda)
 
-	% Initialize values...
+	% number of training examples
 	m = length(y); 
+	% Initializing J and grad...
 	J = 0;
-	initial_theta = zeros(size(X, 2), 1);
-	grad = zeros(size(initial_theta));
+	grad = zeros(size(theta));
 	
-	predictors = sigmoid(X * initial_theta);
+	predictors = sigmoid(X * theta);
 	% theta_0 (index = 1) is omitted from regularization...
-	J = (-1/m) * sum(y .* log(predictors) + (1 - y) .* log(1 - predictors))+ lambda/(2*m) * sum(initial_theta(2:size(X,2)) .^2);
+	J = (-1/m) * sum(y .* log(predictors) + (1 - y) .* log(1 - predictors))+ lambda/(2*m) * sum(theta(2:size(X,2)) .^2);
+	
 	grad(1) = 1/m * ((predictors - y)' * X(:,1));
-	grad(2:size(theta,1)) = 1/m * ((predictors - y)' * X(:,2:size(X,2)) + lambda * initial_theta(2:size(initial_theta,1))');
+	grad(2:size(theta,1)) = 1/m * ((predictors - y)' * X(:,2:size(X,2)) + lambda * theta(2:size(theta,1))');
 	% Transpose grad so it has the same dimensions as theta
 	grad = grad';	
 	
-end
+end;
 
-% Compute and display initial cost and gradient for regularized logistic
-% regression
-[cost, grad] = costFunctionReg(initial_theta, X, y, lam);
+function p = predict(theta, X)
+	m = size(X, 1); % Number of training examples
+	% initialize p...
+	p = zeros(m, 1);
 
-fprintf('Cost at initial theta (zeros): %f\n', cost);
-
-fprintf('\nProgram paused. Press enter to continue.\n');
-pause;
-
-% Part 2: Regularization and Accuracies
-% Try the following values of lambda (0, 1, 10, 100) and note impact
-% on decision boundary, training set accuracy.
+	for i = 1:m
+		if sigmoid(X(i,:)*theta)>= 0.5,
+			p(i) = 1;
+		elseif sigmoid(X(i,:)*theta)< 0.5,
+			p(i) = 0;
+		else
+			disp('Error: sigmoid function failure')
+		end;
+	end;
+end;
 
 function plotDecisionBoundary(theta, X, y)
 	% Plot Data
@@ -154,24 +95,79 @@ function plotDecisionBoundary(theta, X, y)
 	end
 	hold off
 
-end
+end;
 
-function p = predict(theta, X)
-	m = size(X, 1); % Number of training examples
-	% initialize p...
-	p = zeros(m, 1);
+%% Load Data
+%  The first two columns contains the X values and the third column
+%  contains the label (y).
 
-	for i = 1:m
-		if sigmoid(X(i,:)*theta)>= 0.5,
-			p(i) = 1;
-		elseif sigmoid(X(i,:)*theta)< 0.5,
-			p(i) = 0;
-		else
-			disp('Error: sigmoid function failure')
-		end
-	end
-end
+data = load('ex2data2.txt');
+X = data(:, [1, 2]); y = data(:, 3);
 
+plotData(X, y);
 
+% Put some labels 
+hold on;
+
+% Labels and Legend
+xlabel('Microchip Test 1')
+ylabel('Microchip Test 2')
+
+% Specified in plot order
+legend('y = 1', 'y = 0')
+hold off;
+
+% Add Polynomial Features
+
+% Note that mapFeature also adds a column of ones for us, so the intercept
+% term is handled
+X = mapFeature(X(:,1), X(:,2));
+
+% Initialize fitting parameters
+initial_theta = zeros(size(X, 2), 1);
+
+% Set regularization parameter lambda to 1
+lambda = 1;
+
+% Compute and display initial cost and gradient for regularized logistic
+% regression
+[cost, grad] = costFunctionReg(initial_theta, X, y, lambda);
+
+fprintf('Cost at initial theta (zeros): %f\n', cost);
+
+fprintf('\nProgram paused. Press enter to continue.\n');
+pause;
+
+%% Regularization and Accuracies...
+
+% Initialize fitting parameters
+initial_theta = zeros(size(X, 2), 1);
+
+% Set regularization parameter lambda to 1 (you should vary this)
+lambda = 1;
+
+% Set Options
+options = optimset('GradObj', 'on', 'MaxIter', 400);
+
+% Optimize
+[theta, J, exit_flag] = ...
+	fminunc(@(t)(costFunctionReg(t, X, y, lambda)), initial_theta, options);
+
+% Plot Boundary
+plotDecisionBoundary(theta, X, y);
+hold on;
+title(sprintf('lambda = %g', lambda))
+
+% Labels and Legend
+xlabel('Microchip Test 1')
+ylabel('Microchip Test 2')
+
+legend('y = 1', 'y = 0', 'Decision boundary')
+hold off;
+
+% Compute accuracy on our training set
+p = predict(theta, X);
+
+fprintf('Train Accuracy: %f\n', mean(double(p == y)) * 100);
 
 
